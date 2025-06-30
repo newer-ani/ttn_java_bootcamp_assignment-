74893b7861e14986c268badfb387228e6bdbb72c
Animesh Yadav <animesh.yadav@tothenew.com>
2025-06-30 22:57:30 +0530

    added question 3

---
+++ src/Q3.java
@@ -0,0 +1,14 @@
+//Q3 Use Of method to create List, Set and Map
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Q3 {
+    public static void main(String[] args) {
+
+        List<Integer> list = List.of(1,2,34,6,8);
+        Set<String> set = Set.of("abhi","nidhi","disha");
+        Map<Integer,String> map = Map.of(1,"this",2,"is",3,"an",4,"illusion");
+    }
+}
