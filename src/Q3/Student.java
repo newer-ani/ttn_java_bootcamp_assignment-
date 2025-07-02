b0414a79a943a5b79a5cd915894c3f158ddd86d3
Animesh Yadav <animesh.yadav@tothenew.com>
2025-07-02 16:25:27 +0530

    added question 3

---
+++ src/Q3/Student.java
@@ -0,0 +1,18 @@
+package Q3;
+
+import java.util.Objects;
+
+record Student(Integer id, String name, Integer standard ) {
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(id, student.id) && Objects.equals(name, student.name) && Objects.equals(standard, student.standard);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, standard);
+    }
+}
---
+++ src/Q3/Test.java
@@ -0,0 +1,14 @@
+package Q3;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Student student1 = new Student(1, "Shantanu  Aggarwal", 10);
+        Student student2 = new Student(2, "Shantanu Aggarwal", 12);
+
+        System.out.println(student1);
+        System.out.println(student1.equals(student2));
+        System.out.println(student1.hashCode());
+        System.out.println(student2.hashCode());
+    }
+}
