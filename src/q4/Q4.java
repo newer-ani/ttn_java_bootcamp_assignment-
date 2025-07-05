aaa38b51f982213dc7f949ddff2038dff2b509c4
Animesh Yadav <animesh.yadav@tothenew.com>
2025-07-05 15:53:21 +0530

    added question 4

---
+++ src/q4/Q4.java
@@ -0,0 +1,24 @@
+package q4;
+
+import java.util.Random;
+import java.util.concurrent.*;
+
+public class Q4 {
+
+    public static void main(String[] args) {
+        try(ExecutorService executor = Executors.newSingleThreadExecutor();) {
+            Callable<Integer> task = () -> {
+                Random random = new Random();
+                int value = random.nextInt();
+                return value;
+            };
+            Future<Integer> future = executor.submit(task);
+            Integer result = future.get(); // Blocks until result is available
+            System.out.println("Returned from Future: " + result);
+        }
+        catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
