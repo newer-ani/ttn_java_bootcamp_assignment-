bf728ac96b27f517cd66ae8ceb7a7098cb30be77
Animesh Yadav <animesh.yadav@tothenew.com>
2025-07-15 15:55:27 +0530

    changed the packaging type to pom in the parent and then used command line to create the child directories  using maven

---
+++ child2/src/main/java/com/ttn/maven/App.java
@@ -0,0 +1,13 @@
+package com.ttn.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
